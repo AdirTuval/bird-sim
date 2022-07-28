8365f1a44af96e6bef7589bf8e78f5df91789889
AdirTuval <adirtuval@gmail.com>
2022-07-28 14:38:34 +0300

    floor is bigger

--- floor.py
+++ floor.py
@@ -8,7 +8,7 @@ class Floor():
     def __init__(self, space, space_width) -> None:
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
         self.body.position = space_width/2, (FLOOR_HEIGHT/2)
-        self.shape = pymunk.Poly.create_box(self.body, (space_width, FLOOR_HEIGHT))
+        self.shape = pymunk.Poly.create_box(self.body, (space_width * 10, FLOOR_HEIGHT))
         self.shape.elasticity = FLOOR_ELASTICITY
         self.shape.friction = FLOOR_FRICTION
         space.add(self.body, self.shape)
