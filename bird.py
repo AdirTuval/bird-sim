0c52fb5121834e48db1d5ebb18a61dcc417b2e25
yairfine <yair.yfs@gmail.com>
2022-08-01 18:12:32 +0300

    capture continues key-pres for smooth movements. calculate lift.

--- bird.py
+++ bird.py
@@ -2,12 +2,11 @@ import pymunk
 import math
 from constants import *
 
-WING_FORCE = 300
 
 class Bird():
-    RECT_WIDTH = 50
-    RECT_HEIGHT = 100
-    MASS = 3
+    WIDTH = 50
+    HEIGHT = 100
+    MASS = 5
     OPACITY = 100
     COLOR = 255, 0, 0, OPACITY
     OFFSET = 10
@@ -16,28 +15,30 @@ class Bird():
 
     def __init__(self, space, x_location) -> None:
         self.body = pymunk.Body()
-        self.body.position = x_location, self.RECT_HEIGHT / 2
+        self.body.position = x_location, self.HEIGHT / 2
         self.origin = (self.body.position, self.body.angle,
                        self.body.velocity, self.body.angular_velocity)
-        self.shape = pymunk.Poly.create_box(self.body, (self.RECT_WIDTH, self.RECT_HEIGHT))
+        self.shape = pymunk.Poly.create_box(self.body, (self.WIDTH, self.HEIGHT))
         self.shape.elasticity = self.ELASTICITY
         self.shape.friction = self.FRICTION
         self.shape.mass = self.MASS
         self.shape.color = self.COLOR
         space.add(self.body, self.shape)
-        self._create_left_wing(space, x_location - self.RECT_WIDTH / 2 - Wing.WIDTH / 2)
-        self._create_right_wing(space, x_location + self.RECT_WIDTH / 2 + Wing.WIDTH / 2)
+        self._create_left_wing(space, x_location - self.WIDTH / 2 - Wing.WIDTH / 2)
+        self._create_right_wing(space, x_location + self.WIDTH / 2 + Wing.WIDTH / 2)
 
     def _create_left_wing(self, space, x_location):
-        self.left_wing = Wing(space, (x_location, self.RECT_HEIGHT))
-        constraint_left = pymunk.PivotJoint(self.body, self.left_wing.body, (-self.RECT_WIDTH / 2 - self.OFFSET, self.RECT_HEIGHT / 2), (Wing.WIDTH / 2, 0))
+        self.left_wing = Wing(space, (x_location, self.HEIGHT))
+        constraint_left = pymunk.PivotJoint(self.body, self.left_wing.body,
+                                            (-self.WIDTH / 2 - self.OFFSET, self.HEIGHT / 2), (Wing.WIDTH / 2, 0))
         limit_left = pymunk.RotaryLimitJoint(self.body, self.left_wing.body, -PI / 2, PI)
         space.add(limit_left)
         space.add(constraint_left)
 
     def _create_right_wing(self, space, x_location):
-        self.right_wing = Wing(space, (x_location, self.RECT_HEIGHT))
-        constraint_right = pymunk.PivotJoint(self.body, self.right_wing.body, (self.RECT_WIDTH / 2 + self.OFFSET, self.RECT_HEIGHT / 2), (-Wing.WIDTH / 2, 0))
+        self.right_wing = Wing(space, (x_location, self.HEIGHT))
+        constraint_right = pymunk.PivotJoint(self.body, self.right_wing.body,
+                                             (self.WIDTH / 2 + self.OFFSET, self.HEIGHT / 2), (-Wing.WIDTH / 2, 0))
         limit_right = pymunk.RotaryLimitJoint(self.body, self.right_wing.body, -PI, PI / 2)
         space.add(limit_right)
         space.add(constraint_right)
@@ -63,17 +64,22 @@ class Bird():
 
     def right_wing_up(self):
         self.right_wing.up()
-    
+
     def _is_left_wing_pos_valid_for_force_application(self):
         return self.left_wing.body.angle < 0.7
 
     def _is_right_wing_pos_valid_for_force_application(self):
         return self.right_wing.body.angle > -0.7
 
+
 class Wing():
     WIDTH = 60
     HEIGHT = 6
-    MASS = 1
+    WING_AREA = WIDTH * HEIGHT
+    MASS = 2
+    STRENGTH = 100
+    FRICTION = 0.7
+    ELASTICITY = 0.2
 
     def __init__(self, space, position) -> None:
         self.body = pymunk.Body()
@@ -83,15 +89,16 @@ class Wing():
         self.shape = pymunk.Segment(self.body, (0 - self.WIDTH / 2, 0),
                                     (self.WIDTH / 2, 0), self.HEIGHT)
         self.shape.mass = self.MASS
-        self.shape.friction = 0.7
+        self.shape.elasticity = self.ELASTICITY
+        self.shape.friction = self.FRICTION
         self.shape.color = YELLOW
         space.add(self.body, self.shape)
 
     def down(self):
-        self.body.apply_impulse_at_local_point((0, -WING_FORCE*3), (0, 0))
+        self.body.apply_impulse_at_local_point((0, -self.STRENGTH))
 
     def up(self):
-        self.body.apply_impulse_at_local_point((0, WING_FORCE*3), (0, 0))
+        self.body.apply_impulse_at_local_point((0, self.STRENGTH / 2))
 
     def re_origin(self):
         (self.body.position, self.body.angle,
--- simulator.py
+++ simulator.py
@@ -1,26 +1,27 @@
+import sys
 import pygame
-import pymunk.pygame_util
+from pymunk import pygame_util
 from camera import Camera
 import pymunk
+from pymunk.vec2d import Vec2d
 from bird import Bird
 from floor import Floor
 from background import Background
 from constants import *
 
-
 pygame.init()
 
-pymunk.pygame_util.positive_y_is_up = True
+pygame_util.positive_y_is_up = True
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 bg = Background(window)
 clock = pygame.time.Clock()
 space = pymunk.Space()
 space.gravity = 0, GRAVITY
-draw_options = pymunk.pygame_util.DrawOptions(window)
+draw_options = pygame_util.DrawOptions(window)
 
 bird = Bird(space, WIDTH / 2)
-zoom = Camera(draw_options, bird, WIDTH, HEIGHT)
+# zoom = Camera(draw_options, bird, WIDTH, HEIGHT)
 floor = Floor(space, WIDTH)
 text = pygame.font.Font(None, 16).render(HELP_TEXT, True, pygame.Color("black"))
 
@@ -40,15 +41,70 @@ def negate_point(p0, p1):
     return p0
 
 
+def lift(m: float, dt: float, dv: Vec2d):
+    down_force = m / dt * dv
+    # lift = negate_point(down_force, position)
+    return -down_force
+
+
 def run_simulation():
+    # DEBUG PRINTS
+    draw_dv = True
+    draw_lift = True
+
     running = True
     run_physics = True
+    dv_left = Vec2d(0, 0)
+    dv_right = Vec2d(0, 0)
+    prev_v_left = Vec2d(0, 0)
+    prev_v_right = Vec2d(0, 0)
+    lift_left = Vec2d(0, 0)
+    lift_right = Vec2d(0, 0)
 
     while running:
         window.fill((0, 0, 0))
         bg.update(bird.body.position)
         bg.render()
 
+        if run_physics:
+            dv_left = bird.left_wing.body.velocity - prev_v_left
+            dv_right = bird.right_wing.body.velocity - prev_v_right
+            prev_v_left = bird.left_wing.body.velocity
+            prev_v_right = bird.right_wing.body.velocity
+
+            lift_right = lift(AIR_MASS, DT, dv_right)
+            lift_left = lift(AIR_MASS, DT, dv_left)
+
+        if draw_dv:
+            pygame.draw.line(window, BLACK,
+                             pygame_util.to_pygame(bird.left_wing.body.position, window),
+                             pygame_util.to_pygame(bird.left_wing.body.position + dv_left * 3, window))
+            pygame.draw.line(window, BLACK,
+                             pygame_util.to_pygame(bird.right_wing.body.position, window),
+                             pygame_util.to_pygame(bird.right_wing.body.position + dv_right * 3, window))
+
+        if draw_lift:
+            pygame.draw.line(window, RED,
+                             pygame_util.to_pygame(bird.left_wing.body.position, window),
+                             pygame_util.to_pygame(bird.left_wing.body.position + lift_left, window))
+            pygame.draw.line(window, RED,
+                             pygame_util.to_pygame(bird.right_wing.body.position, window),
+                             pygame_util.to_pygame(bird.right_wing.body.position + lift_right, window))
+
+        # capture movement keys
+        if pygame.key.get_pressed()[pygame.K_j]:
+            bird.right_wing_down()
+
+        if pygame.key.get_pressed()[pygame.K_k]:
+            bird.right_wing_up()
+
+        if pygame.key.get_pressed()[pygame.K_f]:
+            bird.left_wing_down()
+
+        if pygame.key.get_pressed()[pygame.K_d]:
+            bird.left_wing_up()
+
+        # capture game settings keys
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -58,28 +114,23 @@ def run_simulation():
                 running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
                 pygame.image.save(window, "bird_capture.png")
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_j:
-                bird.right_wing_down()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_k:
-                bird.right_wing_up()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-                bird.left_wing_down()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                bird.left_wing_up()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 run_physics = not run_physics
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 bird.re_origin()
 
-        zoom.update()
+        # zoom.update()
         space.debug_draw(draw_options)
         window.blit(text, (5, 5))
-        bird_height = pygame.font.Font(None, 16).render(str(bird.body.position[1]),True,pygame.Color("red"))
-        window.blit(bird_height, (5,20))
+        bird_height = pygame.font.Font(None, 16).render(str(bird.body.position[1]), True, pygame.Color("red"))
+        window.blit(bird_height, (5, 20))
         pygame.display.update()
         if run_physics:
             space.step(DT)
         clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit(0)
 
 
 if __name__ == '__main__':
