d88760fbc5646e34fd7f82ffe9c81d984905ec3a
Yair Finesilver <yair.yfs@gmail.com>
2022-08-10 18:36:38 +0300

    replace floor

--- floor.py
+++ floor.py
@@ -1,15 +1,20 @@
 import pymunk
+from constants import *
 
-FLOOR_HEIGHT = 10
-FLOOR_ELASTICITY = 0.9
-FLOOR_FRICTION = 0.4
 
 
 class Floor():
+    HEIGHT = 60
+    ELASTICITY = 0.9
+    FRICTION = 0.9
+    COLOR = BROWN
+    AREA_FACTOR = 100
+
     def __init__(self, space, space_width) -> None:
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
-        self.body.position = space_width / 2, (FLOOR_HEIGHT / 2)
-        self.shape = pymunk.Poly.create_box(self.body, (space_width * 10, FLOOR_HEIGHT))
-        self.shape.elasticity = FLOOR_ELASTICITY
-        self.shape.friction = FLOOR_FRICTION
+        self.body.position = space_width / 2, -self.HEIGHT/2
+        self.shape = pymunk.Poly.create_box(self.body, (space_width * self.AREA_FACTOR, self.HEIGHT))
+        self.shape.elasticity = self.ELASTICITY
+        self.shape.friction = self.FRICTION
+        self.shape.color = self.COLOR
         space.add(self.body, self.shape)
