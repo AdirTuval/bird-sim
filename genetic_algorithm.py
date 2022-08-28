dc3d094275df92cc91b75a40d6c03e7aa5d4d4b5
mooshoo55 <shir.schneorson@mail.huji.ac.il>
2022-08-28 20:59:04 +0300

    gentic module ready

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from typing import Mapping, Callable, Tuple, Any, Dict
 
@@ -8,13 +9,18 @@ from simulator import BirdSim
 
 
 class GeneticAlgo():
-    SOLUTION_CURR = 'out/ga4_'
-    bird_sim = BirdSim()
+    SOLUTION_CURR = 'out/ga_'
+    GUI = False
+    SAVE_PROC = False
+    TO_VISUALIZE = 10
 
-    def __init__(self, num_generations=600,
-                 num_parents_mating=50,
+    def __init__(self, gui,
+                 save_proc,
+                 to_visualize,
+                 num_generations,
+                 num_parents_mating,
+                 sol_per_pop,
                  fitness_func: Callable = None,
-                 sol_per_pop=2000,
                  num_genes=120,
                  init_range_low=-1,
                  init_range_high=1,
@@ -41,6 +47,9 @@ class GeneticAlgo():
                              'mutation_percent_genes': mutation_percent_genes,
                              'callback_generation': callback_generation if callback_generation else self.callback_gen,
                              'parallel_processing': parallel_processing}
+        self.GUI = gui
+        self.SAVE_PROC = save_proc,
+        self.TO_VISUALIZE = to_visualize
 
     @staticmethod
     def fitness_func(solution: np.ndarray, solutions_index) -> float:
@@ -59,8 +68,13 @@ class GeneticAlgo():
     def callback_gen(ga_instance: pygad.GA):
         print("Generation : ", ga_instance.generations_completed)
         print("Fitness of the best solution :", ga_instance.best_solution()[1])
-        if ga_instance.generations_completed % 5 == 0:
-            GeneticAlgo.save_results(ga_instance.best_solution()[0], ga_instance.generations_completed)
+        if ga_instance.generations_completed % GeneticAlgo.TO_VISUALIZE == 0:
+            policy = ga_instance.best_solution()[0]
+            if GeneticAlgo.SAVE_PROC:
+                GeneticAlgo.save_results(policy, ga_instance.generations_completed)
+            if GeneticAlgo.GUI:
+                bird_sim = BirdSim()
+                bird_sim.run_simulation_offline(policy, gui=True)
 
     def run(self) -> Tuple[Any, None, Any]:
         ga_instance = pygad.GA(**self.params)
@@ -71,5 +85,29 @@ class GeneticAlgo():
         return ga_instance.best_solution()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Run Genetic Algorithm and teach Birdy to fly')
+    parser.add_argument('--gui', dest='gui', action='store_true', default=False,
+                        help='activate gui')
+    parser.add_argument('--save_proc', dest='save_proc', action='store_true', default=False,
+                        help='save process')
+    parser.add_argument('--to_visualize', metavar='-v', type=int, nargs='?', default=10,
+                        help='visualize current policy every v iterations')
+    parser.add_argument('--num_generations', metavar='-n', type=int, nargs='?', default=600,
+                        help='how many generations the QLearner should run')
+    parser.add_argument('--sol_per_pop', metavar='-s', type=int, nargs='?', default=2000,
+                        help='size of the population in each generation')
+    parser.add_argument('--num_parents_mating', metavar='-p', type=int, nargs='?', default=50,
+                        help='num of parents mating in each generation')
+
+    return parser
+
+
 if __name__ == '__main__':
-    GeneticAlgo().run()
+    parser = parse_args()
+    args = parser.parse_args()
+    bird_evolution = GeneticAlgo(gui=args.gui, save_proc=args.save_proc, to_visualize=args.to_visualize,
+                                 num_generations=args.num_generations, num_parents_mating=args.num_parents_mating,
+                                 sol_per_pop=args.sol_per_pop)
+    bird_evolution.run()
+
